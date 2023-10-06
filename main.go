0f5b93c4ad73c586074c04f032f40ece014e7e30
Felix <felix83moreno@gmail.com>
2023-10-06 13:18:25 -0400

    Added LCS Worlds Fantasy
    Added LCS Worlds Fantasy

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 //Import Packages
 import (
+	"context"
 	"database/sql"
 	"encoding/json"
 	"fmt"
@@ -16,18 +17,55 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/PuerkitoBio/goquery"
+	"github.com/bwmarrin/discordgo"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gocolly/colly"
 	"github.com/google/uuid"
-
-	"github.com/bwmarrin/discordgo"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/sheets/v4"
 )
 
 type Config struct {
-	Token        string `json:"Token"`
-	SQLPassword  string `json:"SQLPassword"`
-	RapidAPIKey  string `json:"X-RapidAPI-Key"`
-	RapidAPIHost string `json:"X-RapidAPI-Host"`
+	Token         string `json:"Token"`
+	SQLPassword   string `json:"SQLPassword"`
+	RapidAPIKey   string `json:"X-RapidAPI-Key"`
+	RapidAPIHost  string `json:"X-RapidAPI-Host"`
+	SpreadsheetID string `json:""SpreadsheetID""`
+
+	AlexNames    []string `json:"AlexNames"`
+	AlexIDs      []string `json:"AlexIDs"`
+	PatrickNames []string `json:"PatrickNames"`
+	PatrickIDs   []string `json:"PatrickIDs"`
+	KrikorNames  []string `json:"KrikorNames"`
+	KrikorIDs    []string `json:"KrikorIDs"`
+	FelixNames   []string `json:"FelixNames"`
+	FelixIDs     []string `json:"FelixIDs"`
+	AlecNames    []string `json:"AlecNames"`
+	AlecIDs      []string `json:"AlecIDs"`
+	GabrielNames []string `json:"GabrielNames"`
+	GabrielIDs   []string `json:"GabrielIDs"`
+	KennethNames []string `json:"KennethNames"`
+	KennethIDs   []string `json:"KennethIDs"`
+	QuincyNames  []string `json:"QuincyNames"`
+	QuincyIDs    []string `json:"QuincyIDs"`
+
+	AlexNamesPlayin    []string `json:"AlexNamesPlayin"`
+	AlexIDsPlayin      []string `json:"AlexIDsPlayin"`
+	PatrickNamesPlayin []string `json:"PatrickNamesPlayin"`
+	PatrickIDsPlayin   []string `json:"PatrickIDsPlayin"`
+	KrikorNamesPlayin  []string `json:"KrikorNamesPlayin"`
+	KrikorIDsPlayin    []string `json:"KrikorIDsPlayin"`
+	FelixNamesPlayin   []string `json:"FelixNamesPlayin"`
+	FelixIDsPlayin     []string `json:"FelixIDsPlayin"`
+	AlecNamesPlayin    []string `json:"AlecNamesPlayin"`
+	AlecIDsPlayin      []string `json:"AlecIDsPlayin"`
+	GabrielNamesPlayin []string `json:"GabrielNamesPlayin"`
+	GabrielIDsPlayin   []string `json:"GabrielIDsPlayin"`
+	KennethNamesPlayin []string `json:"KennethNamesPlayin"`
+	KennethIDsPlayin   []string `json:"KennethIDsPlayin"`
+	QuincyNamesPlayin  []string `json:"QuincyNamesPlayin"`
+	QuincyIDsPlayin    []string `json:"QuincyIDsPlayin"`
 }
 
 type Outcomes_Info struct {
@@ -52,7 +90,8 @@ type Match_Info struct {
 	Bookmarkers []Bookmarkers_Info `json:"bookmakers"`
 }
 
-const chatString = "795763169191788585"
+const chatString = "1159737801647079475"
+const spreadsheetID = "1qC-yEWpy304or_rcPRA06QLqCy5WbO1ZqWOsDmO4NRs"
 
 var db *sql.DB
 
@@ -113,6 +152,15 @@ func main() {
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
+	var config Config
+	configFile, err := os.Open("config.json")
+	defer configFile.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+	jsonParser := json.NewDecoder(configFile)
+	jsonParser.Decode(&config)
+
 	//If session ID matches dont do anything
 	if m.Author.ID == s.State.User.ID {
 		return
@@ -120,32 +168,411 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	userInput := m.Content
 	//Parse command
 	if strings.HasPrefix(userInput, "!") {
-		switch userInput {
-		case "!ping":
-			s.ChannelMessageSend(m.ChannelID, "pong") //Check if someone types hello in chat
-		case "!profile": //Command for Profile info (Currency balance)
-			{
-				balance := getBalance(m.Author.ID, s)
-				s.ChannelMessageSend(chatString, "User: "+m.Author.Username+"\nBalance: "+strconv.Itoa(balance))
-			}
-		// case "!odds":
-		// 	GetOdds(s)
-		case "!register":
-			registerUser(m.Author.ID, m.Author.Username, s)
-		case "!update":
-			updateBalance(m.Author.ID, s)
-		case "!scores":
-			getPastGames(s)
-		case "!games":
-			getTodaysGames(s)
-		default: //!bet, ....
-			{
-				betNBAGame(m.Author.ID, userInput, s)
-				return
+		parts := strings.Split(userInput, " ")
+		if len(parts) > 1 && parts[0] == "!worlds" {
+			switch strings.ToLower(parts[1]) {
+			case "alex":
+				updateWorlds(s, config.AlexNames, config.AlexIDs, "alex", true)
+			case "patrick":
+				updateWorlds(s, config.PatrickNames, config.PatrickIDs, "patrick", true)
+			case "krikor":
+				updateWorlds(s, config.KrikorNames, config.KrikorIDs, "krikor", true)
+			case "felix":
+				updateWorlds(s, config.FelixNames, config.FelixIDs, "felix", true)
+			case "alec":
+				updateWorlds(s, config.AlecNames, config.AlecIDs, "alec", true)
+			case "gabriel":
+				updateWorlds(s, config.GabrielNames, config.GabrielIDs, "gabriel", true)
+			case "kenneth":
+				updateWorlds(s, config.KennethNames, config.KennethIDs, "kenneth", true)
+			case "quincy":
+				updateWorlds(s, config.QuincyNames, config.QuincyIDs, "quincy", true)
+			// case "test":
+			// 	insertInSheet(speadsheetID, "69")
+			default:
+				getWorldsID(s, parts[1], true)
 			}
 		}
+		if len(parts) > 1 && parts[0] == "!playin" {
+			switch strings.ToLower(parts[1]) {
+			case "alex":
+				updateWorlds(s, config.AlexNamesPlayin, config.AlexIDsPlayin, "alex", false)
+			case "patrick":
+				updateWorlds(s, config.PatrickNamesPlayin, config.PatrickIDsPlayin, "patrick", false)
+			case "krikor":
+				updateWorlds(s, config.KrikorNamesPlayin, config.KrikorIDsPlayin, "krikor", false)
+			case "felix":
+				updateWorlds(s, config.FelixNamesPlayin, config.FelixIDsPlayin, "felix", false)
+			case "alec":
+				updateWorlds(s, config.AlecNamesPlayin, config.AlecIDsPlayin, "alec", false)
+			case "gabriel":
+				updateWorlds(s, config.GabrielNamesPlayin, config.GabrielIDsPlayin, "gabriel", false)
+			case "kenneth":
+				updateWorlds(s, config.KennethNamesPlayin, config.KennethIDsPlayin, "kenneth", false)
+			case "quincy":
+				updateWorlds(s, config.QuincyNamesPlayin, config.QuincyIDsPlayin, "quincy", false)
+			default:
+				getWorldsID(s, parts[1], false)
+			}
+
+			// } else {
+			// switch strings.ToLower(userInput) {
+			// case "!ping":
+			// 	s.ChannelMessageSend(m.ChannelID, "pong") //Check if someone types hello in chat
+			// case "!profile": //Command for Profile info (Currency balance)
+			// 	{
+			// 		balance := getBalance(m.Author.ID, s)
+			// 		s.ChannelMessageSend(chatString, "User: "+m.Author.Username+"\nBalance: "+strconv.Itoa(balance))
+			// 	}
+			// // case "!odds":
+			// // 	GetOdds(s)
+			// case "!register":
+			// 	registerUser(m.Author.ID, m.Author.Username, s)
+			// case "!update":
+			// 	updateBalance(m.Author.ID, s)
+			// case "!scores":
+			// 	getPastGames(s)
+			// case "!games":
+			// 	getTodaysGames(s)
+			// default: //!bet, ....
+			// 	{
+			// 		betNBAGame(m.Author.ID, userInput, s)
+			// 		return
+			// 	}
+			// }
+		}
+	}
+}
+
+func getWorldsID(s *discordgo.Session, playerName string, mainEvent bool) {
+	id := "NOT FOUND"
+	listOfMatchURL := []string{}
+	url := "https://gol.gg/players/list/season-ALL/split-ALL/tournament-World%20Championship%202022/"
+	if !mainEvent {
+		url = "https://gol.gg/players/list/season-ALL/split-ALL/tournament-World%20Championship%20Play-In%202022/"
+	}
+	c := colly.NewCollector(colly.AllowedDomains("www.gol.gg", "gol.gg"))
+
+	//Before HTTP request
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting %s\n", r.URL)
+	})
+
+	c.OnHTML("table.table_list tbody tr", func(h *colly.HTMLElement) {
+		h.DOM.Find("a").Each(func(_ int, s *goquery.Selection) {
+			link, exists := s.Attr("href")
+			name := s.Text()
+			if exists {
+				listOfMatchURL = append(listOfMatchURL, link)
+				if strings.EqualFold(name, playerName) {
+					id = strings.TrimSpace((strings.Split(link, "/"))[2])
+					return
+				}
+			}
+		})
+	})
+
+	//Error occured during request
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Error")
+	})
+
+	c.Visit(url)
+	if id == "NOT FOUND" {
+		s.ChannelMessageSend(chatString, "Not found")
+
+	} else {
+		points := getWorldsStats(s, id, playerName, true, mainEvent)
+		msg := fmt.Sprintf("# Player: %s Total Points: %.2f", playerName, points)
+		s.ChannelMessageSend(chatString, msg)
+	}
+}
+
+func updateWorlds(s *discordgo.Session, listUsers []string, list []string, user string, mainEvent bool) {
+	for index, value := range list {
+		points := getWorldsStats(s, value, listUsers[index], false, mainEvent)
+		msg := fmt.Sprintf("### Player: %s Points: %.2f", listUsers[index], points)
+		insertInSheet(spreadsheetID, points, user, index+3, mainEvent)
+		s.ChannelMessageSend(chatString, msg)
+	}
+}
+
+func getWorldsStats(s *discordgo.Session, id string, playerName string, infoFlag bool, mainEvent bool) float64 {
+	totalPoints := 0.0
+	list := []string{}
+	listOfMatchURL := []string{}
+	skip := false
+	resultIndex := 0
+	time := ""
+	result := ""
+
+	url := "https://gol.gg/players/player-matchlist/" + id + "/season-ALL/split-ALL/tournament-World%20Championship%202022/"
+	if !mainEvent {
+		url = "https://gol.gg/players/player-matchlist/" + id + "/season-ALL/split-ALL/tournament-World%20Championship%20Play-In%202022/"
+	}
+	c := colly.NewCollector(colly.AllowedDomains("www.gol.gg", "gol.gg"))
+
+	//Before HTTP request
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting %s\n", r.URL)
+	})
+
+	c.OnHTML("table.table_list tbody tr", func(h *colly.HTMLElement) {
+		h.DOM.Find("td.text-center").Each(func(_ int, s *goquery.Selection) {
+			list = append(list, s.Text())
+			link, exists := s.Find("a").Attr("href")
+			if exists {
+				if !skip { //Skips 2nd link
+					listOfMatchURL = append(listOfMatchURL, link)
+					skip = true
+				} else {
+					skip = false
+				}
+			}
+		})
+	})
+
+	//Error occured during request
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Error")
+	})
+
+	c.Visit(url)
+	for _, value := range listOfMatchURL {
+		points, msg := checkGameStats(value, playerName)
+		result = list[resultIndex]
+		if result == "Victory" {
+			msg += " *Win bonus +2* "
+			points += 2
+			resultIndex += 2
+			time = strings.TrimSpace(list[resultIndex])
+			if convertTime(time) {
+				msg += " *Time bonus +2* "
+				points += 2
+			}
+		} else {
+			resultIndex += 2
+			time = strings.TrimSpace(list[resultIndex])
+		}
+		resultIndex += 4
+		msg += "\n**Result:** " + result + " **Duration:** " + time
+		msg += "\n**POINTS EARNED: **" + strconv.FormatFloat(points, 'f', 2, 64)
+		if infoFlag {
+			s.ChannelMessageSend(chatString, msg)
+		}
+		totalPoints += points
+	}
+	return totalPoints
+}
+
+func checkGameStats(linkString string, player string) (float64, string) {
+	kda, total, msg := "0/0/0", 0.0, ""
+	url := "https://gol.gg/" + linkString[2:]
+	c := colly.NewCollector(colly.AllowedDomains("www.gol.gg", "gol.gg"))
+	list := []string{}
+	listTeamInfo := []string{}
+	side := 0
+	listFB := []bool{}
+	killBonus := 0.0
+	assistBonus := 0.0
+	listFormatString := []string{}
+	listFormatFloat := []float64{}
+	//Before HTTP request
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Printf("Visiting %s\n", r.URL)
+		listFormatString = append(listFormatString, player, url)
+	})
+
+	c.OnHTML("div.row table.playersInfosLine tbody", func(h *colly.HTMLElement) {
+		h.DOM.Find("td:not([colspan])").Each(func(_ int, s *goquery.Selection) {
+			//Find what side player is on
+			text := s.Find("a").Text()
+			if len(text) > 1 {
+				list = append(list, text)
+			}
+			styleAttr, exists := s.Attr("style")
+			if exists && strings.Contains(styleAttr, "text-align") {
+				text := s.Text()
+				list = append(list, text)
+			}
+		})
+	})
+
+	c.OnHTML("div.col-2", func(h *colly.HTMLElement) {
+		h.DOM.Each(func(_ int, s *goquery.Selection) {
+			text := s.Text()
+			if len(text) > 1 {
+				listTeamInfo = append(listTeamInfo, strings.TrimSpace(text))
+			}
+		})
+	})
+
+	c.OnHTML("div.col-2", func(h *colly.HTMLElement) {
+		h.DOM.Find("img").Each(func(_ int, s *goquery.Selection) {
+			altAttr, exists := s.Attr("alt")
+			if exists && altAttr == "First Blood" {
+				listFB = append(listFB, true) // Blue = 1, Red = 10
+			} else {
+				listFB = append(listFB, false)
+			}
+		})
+	})
+
+	//Error occured during request
+	c.OnError(func(_ *colly.Response, err error) {
+		fmt.Println("Error")
+	})
+
+	c.Visit(url)
+
+	for index, value := range list {
+		if strings.EqualFold(strings.ToLower(value), strings.ToLower(player)) {
+			if index <= 14 {
+				side = 0
+			} else {
+				side = 7
+			}
+			kda = list[index+1]
+			minions := strings.TrimSpace(list[index+2])
+			fbFlag := (listFB[1] && side == 0) || (!listFB[1] && side == 7)
+			listFormatString = append(listFormatString, kda, minions, listTeamInfo[side+1], listTeamInfo[side+2], listTeamInfo[side+3], strconv.FormatBool(fbFlag))
+			kills, _ := strconv.ParseFloat((strings.Split(kda, "/"))[0], 64)
+			if kills > 10.0 {
+				killBonus = 2.0
+			}
+			kills = kills * 3
+			deaths, _ := strconv.ParseFloat((strings.Split(kda, "/"))[1], 64)
+			assists, _ := strconv.ParseFloat((strings.Split(kda, "/"))[2], 64)
+			if assists > 10.0 {
+				assistBonus = 2.0
+			}
+			assists = assists * 2
+			minionsFloat, _ := strconv.ParseFloat(minions, 64)
+			minionsFloat = minionsFloat * 0.02
+			towers, _ := strconv.ParseFloat(listTeamInfo[side+1], 64)
+			dragons, _ := strconv.ParseFloat(listTeamInfo[side+2], 64)
+			dragons = dragons * 2
+			barons, _ := strconv.ParseFloat(listTeamInfo[side+3], 64)
+			barons = barons * 3
+			fb := 0.0
+			if fbFlag {
+				fb = 2.0
+			}
+			listFormatFloat = append(listFormatFloat, kills, deaths, assists, minionsFloat, towers, dragons, barons)
+
+			total += kills + assists + minionsFloat + towers + dragons + barons + fb + killBonus + assistBonus
+			total -= deaths
+			msg = formatStatMsg(listFormatString, listFormatFloat)
+			break
+		}
+	}
+	return total, msg
+}
+
+func insertInSheet(spreadsheetID string, points float64, user string, index int, mainEvent bool) {
+	type Credentials struct {
+		Token string `json:"token"`
+	}
+	credentialsJSON, err := ioutil.ReadFile("credentials.json")
+	if err != nil {
+		fmt.Println("Error reading credentials file:", err)
+		return
 	}
 
+	// Create a Credentials struct to hold the data
+	var credentials Credentials
+
+	// Unmarshal the JSON into the Credentials struct
+	err = json.Unmarshal(credentialsJSON, &credentials)
+	if err != nil {
+		fmt.Println("Error unmarshalling JSON:", err)
+		return
+	}
+
+	// Load your credentials
+	creds, err := google.JWTConfigFromJSON([]byte(credentialsJSON), sheets.SpreadsheetsScope)
+	if err != nil {
+		log.Fatalf("Unable to read client secret file: %v", err)
+	}
+
+	// Create a new JWT client
+	client := creds.Client(context.Background())
+
+	// Initialize the Sheets API client
+	srv, err := sheets.New(client)
+	if err != nil {
+		log.Fatalf("Unable to retrieve Sheets client: %v", err)
+	}
+
+	rangeToWrite := "Main!"
+	if !mainEvent {
+		index += 10
+	}
+	switch user {
+	case "alex":
+		rangeToWrite += "C" + strconv.Itoa(index)
+	case "patrick":
+		rangeToWrite += "E" + strconv.Itoa(index)
+	case "krikor":
+		rangeToWrite += "G" + strconv.Itoa(index)
+	case "felix":
+		rangeToWrite += "I" + strconv.Itoa(index)
+	case "alec":
+		rangeToWrite += "K" + strconv.Itoa(index)
+	case "gabriel":
+		rangeToWrite += "M" + strconv.Itoa(index)
+	case "kenneth":
+		rangeToWrite += "O" + strconv.Itoa(index)
+	case "quincy":
+		rangeToWrite += "Q" + strconv.Itoa(index)
+	}
+	//Alex C3-C9 Patrick E3-E9 Krikor G3-G9 Felix I3-I9
+	//Alec K3-K9 Gabriel M3-M9 Kenneth O3-O9 Quincy Q3-Q9
+
+	//Alex C13-C17 Patrick E13-E17 Krikor G13-G17 Felix I13-I17
+	//Alec K13-K17 Gabriel M13-M17 Kenneth O13-O17 Quincy Q13-Q17
+	value := points
+
+	valueRange := &sheets.ValueRange{
+		Values: [][]interface{}{{value}},
+	}
+
+	_, err = srv.Spreadsheets.Values.Update(spreadsheetID, rangeToWrite, valueRange).
+		ValueInputOption("RAW").
+		Do()
+	if err != nil {
+		log.Fatalf("Unable to update value: %v", err)
+	}
+
+	log.Println("Value updated successfully.")
+}
+
+func formatStatMsg(listFormatString []string, listFormatFloat []float64) string {
+	for _, v := range listFormatFloat {
+		listFormatString = append(listFormatString, fmt.Sprintf("%f", v))
+	}
+	msg := "** " + strings.ToUpper(listFormatString[0]) + " STATS **<" + listFormatString[1] + ">\n" +
+		">>> __**PLAYER STATS:**__\n" +
+		"KDA: " + listFormatString[2] + "\n" +
+		"MINIONS: " + listFormatString[3] + "\n\n" +
+		"__**TEAM STATS:**__\n" +
+		"TOWERS: " + listFormatString[4] + " DRAGONS: " + listFormatString[5] +
+		" BARONS: " + listFormatString[6] + " FB: " + listFormatString[7] + "\n\n" +
+		"__**POINT DISTRIBUTION:**__\n" +
+		"K: " + listFormatString[8][:len(listFormatString[8])-4] + " D: -" + listFormatString[9][:len(listFormatString[9])-4] +
+		" A: " + listFormatString[10][:len(listFormatString[10])-4] + " M: " + listFormatString[11][:len(listFormatString[11])-4] + "\n" +
+		"T: " + listFormatString[12][:len(listFormatString[12])-4] + " DG: " + listFormatString[13][:len(listFormatString[13])-4] +
+		" B: " + listFormatString[14][:len(listFormatString[14])-4] + "\n"
+	if listFormatFloat[0]/3 > 10 {
+		msg += " *Kill Bonus +2* "
+	}
+	if listFormatFloat[2]/2 > 10 {
+		msg += " *Assist Bonus +2* "
+	}
+	if listFormatString[7] == "true" {
+		msg += " *FB Bonus +2* "
+	}
+	return msg
 }
 
 func registerUser(id string, username string, s *discordgo.Session) {
@@ -438,7 +865,7 @@ func betNBAGame(id string, input string, s *discordgo.Session) {
 	}
 	numAmount, err := strconv.Atoi(amount)
 	if err != nil {
-		fmt.Println("Error, not a number")
+		fmt.Println("Error, not a number!")
 		return
 	}
 	//Case if amount bet is over actual amount (Negative)
@@ -478,8 +905,6 @@ func betNBAGame(id string, input string, s *discordgo.Session) {
 			s.ChannelMessageSend(chatString, "Updated Balance to "+strconv.Itoa(currBalance-numAmount)+"\n")
 		}
 	}
-
-	//At 5am EST update for NBA (LCS will be different)
 }
 
 func matchNBATeam(input string) string {
@@ -549,4 +974,32 @@ func matchNBATeam(input string) string {
 		fmt.Println("Error, can't find team")
 		return ""
 	}
+}
+
+func convertTime(s string) bool {
+	// Split the time string into minutes and seconds
+	parts := strings.Split(s, ":")
+	if len(parts) != 2 {
+		fmt.Println("Invalid time format")
+		return false
+	}
+
+	minutes, err := strconv.Atoi(parts[0])
+	if err != nil {
+		fmt.Println("Error converting minutes:", err)
+		return false
+	}
+
+	seconds, err := strconv.Atoi(parts[1])
+	if err != nil {
+		fmt.Println("Error converting seconds:", err)
+		return false
+	}
+
+	// Convert to total seconds
+	totalSeconds := (minutes * 60) + seconds
+	if totalSeconds < 1800 {
+		return true
+	}
+	return false
 }
